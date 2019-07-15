7415829211dd72528e954dbe4e51dab52902ada7
SuichiM <MartiniRoqueEsteban@gmail.com>
2019-07-15 00:14:11 -0300

    added Cart component

---
+++ vue-practice-shop/src/components/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="item in items" :key="item.id">
+      {{ item.title }} - $ {{ item.price }}
+      <button class="btn badge badge-danger float-right">
+          <span class="glyphicon glyphicon-trash">Delete Item</span>
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['items']
+}
+</script>
+
