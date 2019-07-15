14714e0fd8d19787b13bfa007d27d005b7755d98
SuichiM <MartiniRoqueEsteban@gmail.com>
2019-07-14 23:22:00 -0300

    created the Product Component

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-7"></div>
+      <div class="col-md-7">
+        <div class="row">
+          <div :key="product.id" class="col-md-6" v-for="product in products">
+            <product></product>
+          </div>
+        </div>
+      </div>
       <div class="col-md-5"></div>
     </div>
   </div>
 </template>
 
 <script>
+import products from '@/products.json'
+import Product from '@/components/Product.vue'
+
+
 export default {
   name: 'app',
+  components: {
+    Product
+  },
+  data() {
+    return {
+      products
+    }
+  }
 }
 </script>
 
