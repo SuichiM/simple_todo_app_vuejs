8e6ebc7298df7e934fd6039493af77106d00c665
SuichiM <martiniroqueesteban@gmail.com>
2019-10-13 01:41:33 -0300

    added component to createArticle

--- vue-practice-blog/src/router.js
+++ vue-practice-blog/src/router.js
@@ -3,8 +3,8 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
 import Home from './views/Home.vue'
-import Article from './views/Article';
-
+import Article from './views/Article.vue';
+import CreateArticle from './views/CreateArticle.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -21,11 +21,18 @@ const router = new Router({
     path: '/',
     component: Home,
     props: true
-  }, {
+  }, 
+  {
+    path: '/articles/create',
+    component: CreateArticle,
+    props: true
+  },
+  {
     path: '/article/:id',
     component: Article,
     props: true
-  }]
+  },
+]
 })
 
 export default router;
